a531477ff5920e00eddf33fa3f997795364fe747
LakmalSulochana <sulochanalakmal789@gmail.com>
2022-05-15 03:52:32 +0530

    code done set 2 in empty tiles

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -144,3 +144,21 @@ function slideDown() {
         }
     }
 }
+function setTwo() {
+    if (!hasEmptyTile()) {
+        return;
+    }
+    let found = false;
+    while (!found) {
+        //find random row and column to place a 2 in
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+        }
+    }
+}
